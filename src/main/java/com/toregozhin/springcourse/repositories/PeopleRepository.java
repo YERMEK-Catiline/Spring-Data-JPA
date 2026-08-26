72cbfb0d634b25fcf7eaeba9e37c4c70ca5baab7
CATILINA <ermektoregozhin@gmail.com>
2026-08-26 08:10:40 +0500

    feat: add Person model, PeopleRepository and hibernate properties

---
+++ src/main/java/com/toregozhin/springcourse/repositories/PeopleRepository.java
@@ -0,0 +1,10 @@
+package com.toregozhin.springcourse.repositories;
+
+import com.toregozhin.springcourse.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+}
